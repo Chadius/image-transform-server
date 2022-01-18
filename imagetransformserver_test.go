715cc22e8b90db8d1b224c66208a2c2bc8d92d79
Chad Serrant <therealchadius@gmail.com>
2022-01-18 13:15:31 -0500

    Split test concerns.

--- imagetransformserver_test.go
+++ imagetransformserver_test.go
@@ -21,61 +21,30 @@ func TestExampleTestSuite(t *testing.T) {
 
 type UsePackageTestSuite struct {
 	suite.Suite
+	request                         *http.Request
+	responseRecorder                *httptest.ResponseRecorder
+	server                          image_transform_server.TwirpServer
+	fakeTransformPackage            *creatingsymmetryfakes.FakeTransformerStrategy
+	inputImageData                  []byte
+	formulaData                     []byte
+	outputSettingsData              []byte
+	fakeTransformPackageReturnValue []byte
 }
 
-func (suite *UsePackageTestSuite) TestWhenFilesAreSupplied_ThenCallPackage() {
-	// Setup
-	imageData := []byte(`images go here`)
-	formulaData := []byte(`formula goes here`)
-	outputSettingsData := []byte(`outputSettings go here`)
-	requestBody := suite.getDataStream(imageData, formulaData, outputSettingsData)
-	testRequest := suite.generateProtobufRequest(requestBody)
+func (suite *UsePackageTestSuite) SetupTest() {
+	suite.inputImageData = []byte(`images go here`)
+	suite.formulaData = []byte(`formula goes here`)
+	suite.outputSettingsData = []byte(`outputSettings go here`)
+	requestBody := suite.getDataStream()
+	suite.request = suite.generateProtobufRequest(requestBody)
+	suite.responseRecorder = httptest.NewRecorder()
 
-	expectedResponse := []byte(`rules responded`)
-	fakeTransformerStrategy := suite.getFakeTransformerStrategyWithResponse(expectedResponse)
-	twirpServer := suite.getTwirpServer(fakeTransformerStrategy)
-
-	responseRecorder := httptest.NewRecorder()
-
-	// Act
-	twirpServer.ServeHTTP(responseRecorder, testRequest)
-
-	// Assert
-	response := responseRecorder.Result()
-
-	require := require.New(suite.T())
-	require.Equal(200, response.StatusCode, "Status code is wrong")
-
-	suite.requireResponseDataMatches(responseRecorder, require, expectedResponse)
-	suite.requireFakePackageWasCalledWithExpectedData(require, fakeTransformerStrategy, imageData, formulaData, outputSettingsData)
+	suite.fakeTransformPackageReturnValue = []byte(`rules responded`)
+	suite.fakeTransformPackage = suite.fakeTransformerStrategyWithResponse(suite.fakeTransformPackageReturnValue)
+	suite.server = suite.getServer()
 }
 
-func (suite *UsePackageTestSuite) requireFakePackageWasCalledWithExpectedData(require *require.Assertions, fakeTransformerStrategy *creatingsymmetryfakes.FakeTransformerStrategy, imageData, formulaData, outputSettingsData []byte) {
-	require.Equal(1, fakeTransformerStrategy.ApplyFormulaToTransformImageCallCount())
-
-	actualInputImageDataByteStream, actualFormulaDataByteStream, actualOutputSettingsDataByteStream, _ := fakeTransformerStrategy.ApplyFormulaToTransformImageArgsForCall(0)
-
-	actualInputImageData, imageReadErr := ioutil.ReadAll(actualInputImageDataByteStream)
-	require.Nil(imageReadErr, "Error while reading input image data from mock object")
-	require.Equal(0, bytes.Compare(imageData, actualInputImageData), "input image given to mock object is different")
-
-	actualFormulaData, formulaReadErr := ioutil.ReadAll(actualFormulaDataByteStream)
-	require.Nil(formulaReadErr, "Error while reading formula data from mock object")
-	require.Equal(0, bytes.Compare(formulaData, actualFormulaData), "formula given to mock object is different")
-
-	actualOutputSettingsData, outputSettingsReadErr := ioutil.ReadAll(actualOutputSettingsDataByteStream)
-	require.Nil(outputSettingsReadErr, "Error while reading output settings data from mock object")
-	require.Equal(0, bytes.Compare(outputSettingsData, actualOutputSettingsData), "output settings given to mock object is different")
-}
-
-func (suite *UsePackageTestSuite) requireResponseDataMatches(responseRecorder *httptest.ResponseRecorder, require *require.Assertions, expectedResponse []byte) {
-	output := &image_transform_server.Image{}
-	unmarshalErr := proto.Unmarshal(responseRecorder.Body.Bytes(), output)
-	require.Nil(unmarshalErr, "Error while unmarshalling response body")
-	require.Equal(expectedResponse, output.ImageData, "output image received from mock object is different")
-}
-
-func (suite *UsePackageTestSuite) getFakeTransformerStrategyWithResponse(expectedResponse []byte) *creatingsymmetryfakes.FakeTransformerStrategy {
+func (suite *UsePackageTestSuite) fakeTransformerStrategyWithResponse(expectedResponse []byte) *creatingsymmetryfakes.FakeTransformerStrategy {
 	fakeTransformerStrategy := creatingsymmetryfakes.FakeTransformerStrategy{}
 	fakeTransformerStrategy.ApplyFormulaToTransformImageStub = func(inputImageDataByteStream, formulaDataByteStream, outputSettingsDataByteStream io.Reader, output io.Writer) error {
 		output.Write(expectedResponse)
@@ -84,9 +53,9 @@ func (suite *UsePackageTestSuite) getFakeTransformerStrategyWithResponse(expecte
 	return &fakeTransformerStrategy
 }
 
-func (suite *UsePackageTestSuite) getTwirpServer(fakeTransformerStrategy *creatingsymmetryfakes.FakeTransformerStrategy) image_transform_server.TwirpServer {
+func (suite *UsePackageTestSuite) getServer() image_transform_server.TwirpServer {
 	server := &transformserver.Server{
-		Transformer: fakeTransformerStrategy,
+		Transformer: suite.fakeTransformPackage,
 	}
 	twirpServer := image_transform_server.NewImageTransformerServer(server)
 	return twirpServer
@@ -104,11 +73,11 @@ func (suite *UsePackageTestSuite) generateProtobufRequest(requestBody *bytes.Buf
 	return testRequest
 }
 
-func (suite *UsePackageTestSuite) getDataStream(imageData []byte, formulaData []byte, outputSettingsData []byte) *bytes.Buffer {
+func (suite *UsePackageTestSuite) getDataStream() *bytes.Buffer {
 	dataStream := &image_transform_server.DataStreams{
-		InputImage:     imageData,
-		FormulaData:    formulaData,
-		OutputSettings: outputSettingsData,
+		InputImage:     suite.inputImageData,
+		FormulaData:    suite.formulaData,
+		OutputSettings: suite.outputSettingsData,
 	}
 
 	protobuf, protobufErr := proto.Marshal(dataStream)
@@ -118,4 +87,62 @@ func (suite *UsePackageTestSuite) getDataStream(imageData []byte, formulaData []
 	require.Nil(protobufErr)
 
 	return requestBody
+}
+
+func (suite *UsePackageTestSuite) TestWhenClientMakesRequest_ResponseIsValid() {
+	// Act
+	suite.server.ServeHTTP(suite.responseRecorder, suite.request)
+
+	// Assert
+	response := suite.responseRecorder.Result()
+
+	require := require.New(suite.T())
+	require.Equal(200, response.StatusCode, "Status code is wrong")
+}
+
+func (suite *UsePackageTestSuite) TestWhenClientMakesRequest_PackageIsCalledWithInputData() {
+	// Act
+	suite.server.ServeHTTP(suite.responseRecorder, suite.request)
+
+	// Assert
+	response := suite.responseRecorder.Result()
+
+	require := require.New(suite.T())
+	require.Equal(200, response.StatusCode, "Status code is wrong")
+
+	suite.requireFakePackageWasCalledWithExpectedData(require)
+}
+
+func (suite *UsePackageTestSuite) requireFakePackageWasCalledWithExpectedData(require *require.Assertions) {
+	require.Equal(1, suite.fakeTransformPackage.ApplyFormulaToTransformImageCallCount())
+
+	actualInputImageDataByteStream, actualFormulaDataByteStream, actualOutputSettingsDataByteStream, _ := suite.fakeTransformPackage.ApplyFormulaToTransformImageArgsForCall(0)
+
+	actualInputImageData, imageReadErr := ioutil.ReadAll(actualInputImageDataByteStream)
+	require.Nil(imageReadErr, "Error while reading input image data from mock object")
+	require.Equal(0, bytes.Compare(suite.inputImageData, actualInputImageData), "input image given to mock object is different")
+
+	actualFormulaData, formulaReadErr := ioutil.ReadAll(actualFormulaDataByteStream)
+	require.Nil(formulaReadErr, "Error while reading formula data from mock object")
+	require.Equal(0, bytes.Compare(suite.formulaData, actualFormulaData), "formula given to mock object is different")
+
+	actualOutputSettingsData, outputSettingsReadErr := ioutil.ReadAll(actualOutputSettingsDataByteStream)
+	require.Nil(outputSettingsReadErr, "Error while reading output settings data from mock object")
+	require.Equal(0, bytes.Compare(suite.outputSettingsData, actualOutputSettingsData), "output settings given to mock object is different")
+}
+
+func (suite *UsePackageTestSuite) TestWhenClientMakesRequest_ResponseIsUnmarshalled() {
+	// Act
+	suite.server.ServeHTTP(suite.responseRecorder, suite.request)
+
+	// Assert
+	require := require.New(suite.T())
+	suite.requireResponseDataMatches(require)
+}
+
+func (suite *UsePackageTestSuite) requireResponseDataMatches(require *require.Assertions) {
+	output := &image_transform_server.Image{}
+	unmarshalErr := proto.Unmarshal(suite.responseRecorder.Body.Bytes(), output)
+	require.Nil(unmarshalErr, "Error while unmarshalling response body")
+	require.Equal(suite.fakeTransformPackageReturnValue, output.ImageData, "output image received from mock object is different")
 }
